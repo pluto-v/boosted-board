352a629717f1272549f244c20af2cbdc30f03e7d
Ningsong Shen <ningsongshen123@outlook.com>
2017-05-30 19:29:31 -0400

    add faded navbar, image fixes

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 /* insert some js here */
+
+(function ($) {
+  $(document).ready(function(){
+
+    // hide .navbar first
+    $(".bar").hide();
+
+    // fade in .navbar
+    $(function () {
+        $(window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ($(this).scrollTop() > 150) {
+                $('.bar').fadeIn();
+            } else {
+                $('.bar').fadeOut();
+            }
+        });
+    });
+
+});
+  }(jQuery));
